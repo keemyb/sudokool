e9c667657eee5eb25248426d54416a301e13011a
Akeem Brown <akeemb@gmail.com>
2014-07-08 20:45:33 +0100

    rewrote str function, still ugly.

---
+++ sudoku.py
@@ -0,0 +1,414 @@
+# -*- coding: cp1252 -*-
+
+#subGridsx = amount of subgrids in X plane
+
+class sudoku():
+        
+    def __init__(self, gridSize, subGridsX, subGridsY, data):
+        self.gridSize = gridSize
+        self.subGridsX = subGridsX
+        self.subGridsY = subGridsY
+        self.data = {position + 1 : int(data[position]) for position in range(gridSize ** 2)}
+        self.ghostData = {}
+        # self.subGridStartLocations = self.getSubGridStartLocations()
+        # self.rowStartLocations = self.getRowStartLocations()
+        # self.columnStartLocations = self.getColumnStartLocations()
+        # self.setOfPossibleNumbers = set(xrange(1, self.gridSize + 1))
+        # self.changes = False
+        # self.intersectionTypes = {"subGrid":[self.subGridStartLocations, self.getSubGridMembers],
+        # "row":[self.rowStartLocations, self.getRowMembers],
+        # "column":[self.columnStartLocations, self.getColumnMembers]}
+
+##        try:
+##            if gridSize % subGridsX != 0 or gridSize % subGridsY != 0:
+##                #return "invalid dimensions"
+##                raise Exception("invalid dimensions")
+##            if gridSize != (subGridsX * subGridsY):
+##                #return "invalid dimensions"
+##                raise Exception("invalid dimensions")
+##        except:
+##            return ValueError
+
+        #self.populateGhosts()            
+        
+    def __repr__(self):
+        pass #rebuildable representation
+
+    def __str__(self):
+        gridSize = self.getGridSize()
+        subGridsX = self.getSubGridsX()
+        subGridsY = self.getSubGridsY()
+        string = ""
+        vpipe = "||"
+        hpipe = "="
+
+        for position in xrange(1, gridSize ** 2 + 1):
+
+            if self.data[position] == 0:
+                string += " "
+            else:
+                string += str(self.data[position])
+
+            if position % gridSize == 0:
+
+                string += "\n"
+
+        return string
+
+    def getSubGridStartLocations(self):
+        subGridStartLocations = []
+        for subGrid in xrange(self.gridSize):
+            subGridStartLocations.append((subGrid * self.gridSize) + 1)
+        return sorted(subGridStartLocations)
+
+    def getRowStartLocations(self):
+        gridSize = self.getGridSize()
+        subGridsX = self.getSubGridsX()
+        subGridsY = self.getSubGridsY()
+        rowStartLocations = []
+
+        for row in xrange(self.gridSize):
+            membersInAboveSubGrids  = (row / subGridsX) * gridSize * subGridsX
+
+            if (row - 1) % subGridsX != 0:
+                adjustment = ((row - 1) % subGridsX) * subGridsY
+            else:
+                adjustment = 0
+
+            rowStartLocation = membersInAboveSubGrids + adjustment + 1
+            
+            rowStartLocations.append(rowStartLocation)
+        return sorted(rowStartLocations)
+
+    def getColumnStartLocations(self):
+        gridSize = self.getGridSize()
+        subGridsX = self.getSubGridsX()
+        subGridsY = self.getSubGridsY()
+        columnStartLocations = []
+
+        for column in xrange(self.gridSize):
+            membersInLeftwardSubGrids = (column / subGridsY) * gridSize
+
+            if (column + 1) % subGridsY != 0:
+                adjustment = ((column + 1) % subGridsY)
+            else:
+                adjustment = 0
+
+            columnStartLocation = membersInLeftwardSubGrids + adjustment + 1
+
+            columnStartLocations.append(columnStartLocation)
+        return sorted(columnStartLocations)
+
+    def indexToStorageLocation(self, index):
+            gridSize = self.gridSize
+            subGridsX = self.subGridsX
+            subGridsY = self.subGridsY
+            index = index + 1
+
+            if index % gridSize == 0: #last number in row
+                subGrid = (((index - 1) / (gridSize * subGridsX)) + 1) * subGridsX
+                columnInSubGrid = subGridsY
+            else:
+                subGrid = ((((index - 1) / (gridSize * subGridsX)) + 1) * subGridsX) - \
+                          (subGridsX - ((((index % gridSize) - 1) / subGridsY) + 1))
+                if index % subGridsY == 0:
+                    columnInSubGrid = subGridsY
+                else:
+                    columnInSubGrid = index % subGridsY
+
+            if index % (gridSize * subGridsX) == 0: #last number of last subGrid in Row
+                rowInSubGrid = subGridsX
+            else:
+                if index % gridSize == 0:
+                    rowInSubGrid = (((index) % (gridSize * subGridsX))) / gridSize
+                else:
+                    rowInSubGrid = ((((index) % (gridSize * subGridsX))) / gridSize) + 1
+
+            if subGrid % subGridsX == 0: #last subGrid in row
+                membersInHorzAdjSubGrids = (subGridsX - 1) * gridSize
+            else:
+                membersInHorzAdjSubGrids = ((subGrid % subGridsX) - 1) * gridSize
+
+            membersInAboveSubGrids  = ((subGrid - 1)/subGridsX) * gridSize * subGridsX
+
+            if rowInSubGrid == 1:
+                membersInAboveRowsInSubGrid = 0
+            else:
+                membersInAboveRowsInSubGrid = (rowInSubGrid - 1) * subGridsY
+
+            membersLeftInRowIncSelf = columnInSubGrid
+
+            position = membersInAboveSubGrids + membersInHorzAdjSubGrids + \
+                       membersInAboveRowsInSubGrid + membersLeftInRowIncSelf
+                
+            return position
+
+    def getGridSize(self):
+        return self.gridSize
+
+    def getSubGridsX(self):
+        return self.subGridsX
+
+    def getSubGridsY(self):
+        return self.subGridsY
+
+    def getData(self):
+        return self.data
+
+    def getSubGrid(self, location):
+        gridSize = self.getGridSize()
+
+        if location % gridSize == 0: #last number in box
+            return location / gridSize
+        else:
+            return (location / gridSize) + 1
+
+    def getPositionInSubGrid(self, location):
+        gridSize = self.getGridSize()
+
+        if location % gridSize == 0: #last number in box
+            return gridSize
+        else:
+            return location % gridSize
+
+    def getRowInSubGrid(self, location):
+        gridSize = self.getGridSize()
+        subGridsY = self.getSubGridsY()
+        subGridsX = self.getSubGridsX()
+
+        if location % gridSize == 0: #last number in box
+            return subGridsX
+        else:
+            return (((location % gridSize) - 1) / subGridsY) + 1
+
+    def getColumnInSubGrid(self, location):
+        gridSize = self.getGridSize()
+        subGridsY = self.getSubGridsY()
+
+        if location % subGridsY == 0: #last number in box
+            return subGridsY
+        else:
+            return ((location % gridSize) % subGridsY)
+
+    def getSubGridMembers(self, location):
+        """returns locations of sub grid members, the location given is included in list"""
+        gridSize = self.getGridSize()
+
+        resolvedMembers = []
+
+        if location % gridSize == 0:
+            firstMember = (location - gridSize) + 1
+        else:
+            firstMember = location - (location % gridSize) + 1
+
+        lastMember = (firstMember + gridSize) - 1
+        subGridMembers = range(firstMember, lastMember + 1)
+
+        for i in subGridMembers:
+            resolvedMembers.append(self.getData()[i])
+
+        return dict(zip(subGridMembers, resolvedMembers))
+
+    def getRowMembers(self, location):
+        gridSize = self.getGridSize()
+        subGridsX = self.getSubGridsX()
+        subGridsY = self.getSubGridsY()
+        rowInSubGrid = self.getRowInSubGrid(location)
+
+        rowMembers = []
+        resolvedMembers = []
+
+        subGridRow = ((self.getSubGrid(location) - 1) / subGridsX) + 1
+        
+        firstSubGridInRow = ((subGridRow - 1) * subGridsX) + 1
+        lastSubGridInRow = firstSubGridInRow + subGridsX - 1
+        subGridsInRow = range(firstSubGridInRow, lastSubGridInRow + 1)
+
+        for subGrid in subGridsInRow:
+            firstMemberInSubGrid = ((subGrid - 1) * gridSize) + 1
+            firstRowMemberInSubGrid = firstMemberInSubGrid + ((rowInSubGrid - 1) * subGridsY)
+            lastRowMemberInSubGrid = firstRowMemberInSubGrid + subGridsY - 1
+            rowMembersInSubGrid = range(firstRowMemberInSubGrid, lastRowMemberInSubGrid + 1)
+
+            for member in rowMembersInSubGrid:
+                rowMembers.append(member)
+                resolvedMembers.append(self.getData()[member])
+            
+        return dict(zip(rowMembers,resolvedMembers))
+
+    def getColumnMembers(self, location):
+        gridSize = self.getGridSize()
+        subGridsX = self.getSubGridsX()
+        subGridsY = self.getSubGridsY()
+        columnInSubGrid = self.getColumnInSubGrid(location)
+
+        columnMembers = []
+        resolvedMembers = []
+        subGridsInColumn = []
+
+        if self.getSubGrid(location) % subGridsX == 0:
+            subGridColumn = subGridsX
+        else:
+            subGridColumn = self.getSubGrid(location) % subGridsX
+
+        firstSubGridInColumn = subGridColumn
+        for i in xrange(subGridsY):
+            subGridsInColumn.append(firstSubGridInColumn + (subGridsX * i))
+
+        for subGrid in subGridsInColumn:
+            firstMemberInSubGrid = ((subGrid - 1) * gridSize) + 1
+            firstColumnMemberInSubGrid = firstMemberInSubGrid + columnInSubGrid - 1
+
+            j = subGridsX
+            while j != 0:
+                reference = firstColumnMemberInSubGrid + subGridsY * (j - 1)
+                columnMembers.append(reference)
+                resolvedMembers.append(self.getData()[reference])
+                j -= 1
+
+        return dict(zip(columnMembers,resolvedMembers))
+
+    def resolveMember(self, location):
+        resolvedMember = self.getData()[location]
+        return resolvedMember
+
+    def populateGhosts(self):
+        for i in xrange(self.gridSize): # subGrid
+            existingValues = []    
+
+            for location, value in self.getSubGridMembers((i * self.gridSize) + 1).iteritems():
+                if value != 0:
+                    existingValues.append(int(value)) # int for diff
+
+            for location, value in self.getSubGridMembers((i * self.gridSize) + 1).iteritems():
+                if self.resolveMember(location) == 0:
+                    setOfNonExistingValues = self.setOfPossibleNumbers.difference(set(existingValues))
+                    self.ghostData[location] = list(setOfNonExistingValues)
+
+        for i in xrange(self.gridSize): #columns
+            existingValues = []
+            reference = ((i / self.subGridsY) * self.gridSize) + ((i + 1) % self.subGridsY)
+
+            for location, value in self.getColumnMembers(reference).iteritems():
+                if value != 0:
+                    existingValues.append(int(value))
+
+            for location, value in self.getColumnMembers(reference).iteritems():
+                if self.resolveMember(location) == 0:
+                    setOfExistingGhostValues = set(self.ghostData[location])
+                    setOfNonExistingValues = self.setOfPossibleNumbers.difference(set(existingValues))
+                    self.ghostData[location] = list(setOfExistingGhostValues.intersection(setOfNonExistingValues))
+
+        for i in xrange(self.gridSize): #rows
+            existingValues = []
+            reference = ((((i + self.subGridsX) / self.subGridsX) - 1) * self.gridSize * self.subGridsX) + \
+                        ((i % self.subGridsX) * self.subGridsY) + 1
+
+            for location, value in self.getRowMembers(reference).iteritems():
+                if value != 0:
+                    existingValues.append(int(value))            
+
+            for location, value in self.getRowMembers(reference).iteritems():
+                if self.resolveMember(location) == 0:
+                    setOfExistingGhostValues = set(self.ghostData[location])
+                    setOfNonExistingValues = self.setOfPossibleNumbers.difference(set(existingValues))
+                    self.ghostData[location] = list(setOfExistingGhostValues.intersection(setOfNonExistingValues))
+
+    def updateGhosts(self):
+        for location, value in self.ghostData.iteritems():
+            if value == []:
+                del self.ghostData[location]
+
+
+                    
+
+
+
+
+    def nakedSingle(self):
+        self.populateGhosts()
+        self.changes = False
+        ghostKeysToDelete = []
+
+        for location, value in self.ghostData.iteritems():
+            if len(value) == 1:
+                self.data[location] = value[0]
+                ghostKeysToDelete.append(location)
+                self.changes = True
+
+        if self.changes:
+            for location in ghostKeysToDelete:
+                del self.ghostData[location]
+            self.populateGhosts()
+
+        return self.changes
+
+    def hiddenSingle(self):
+        self.populateGhosts()
+        self.changes = False
+
+        for intersectionType in self.intersectionTypes.itervalues():
+
+            for startLocation in intersectionType[0]:
+                members = intersectionType[1](startLocation)
+
+                unresolvedLocations = sorted([key for key in members.iterkeys() if
+                                    (key in self.ghostData.keys())])
+                
+                for location in unresolvedLocations:
+                    surroundingLocations = []
+                    for surroundingLocation in unresolvedLocations:
+                        if surroundingLocation != location:
+                            surroundingLocations.append(surroundingLocation)
+                            
+                    surroundingGhosts = []
+                    for surroundingLocation in surroundingLocations:
+                        if surroundingLocation != location:
+                            for ghostValue in self.ghostData[surroundingLocation]:
+                                surroundingGhosts.append(ghostValue)
+
+                    setOfSurroundingGhosts = set(surroundingGhosts)                
+                    locationGhosts = [ghostValue for ghostValue in self.ghostData[location]]
+                    setOfLocationGhosts = set(locationGhosts)
+
+                    setOfUniqueGhosts = setOfLocationGhosts - setOfSurroundingGhosts
+
+                    if len(setOfUniqueGhosts) == 1:
+                        self.data[location] = setOfUniqueGhosts.pop()
+                        del self.ghostData[location]
+                        self.changes = True
+                        self.populateGhosts()
+                        unresolvedLocations = sorted([key for key in members.iterkeys() if
+                                                      (key in self.ghostData.keys())])
+
+    def nakedTwin(self):
+        self.populateGhosts()
+        self.changes = False
+
+        for intersectionType in self.intersectionTypes.itervalues():
+
+            for startLocation in intersectionType[0]:
+                
+                members = intersectionType[1](startLocation)
+
+                unresolvedLocations = sorted([key for key in members.iterkeys() if
+                        (key in self.ghostData.keys())])
+
+                for locationOne in unresolvedLocations:
+
+                    for locationTwo in unresolvedLocations:
+
+                        if locationOne != locationTwo and len(unresolvedLocations) > 2:
+
+                            if len(self.ghostData[locationOne]) == 2 and self.ghostData[locationOne] == self.ghostData[locationTwo]:
+                                for location in unresolvedLocations:
+
+                                    if location != locationOne and location != locationTwo:
+
+                                        for ghostValue in self.ghostData[locationOne]:
+
+                                            if ghostValue in self.ghostData[location]:
+
+                                                self.changes = True
+                                                self.ghostData[location].remove(ghostValue)
+                                                ##self.populateGhosts()
---
+++ tests.py
@@ -0,0 +1,83 @@
+from sudoku import sudoku
+
+def easy(full = True):
+    
+    easy50 = open("easy50.txt", "r")
+    stringEasy = ""
+    for line in easy50:
+        stringEasy += line.strip()
+
+    results = {}
+
+    for i in xrange(50):
+        start = (i * 89)
+        end = start + 9 * 9 
+
+        stringEasy9 = stringEasy[start : end]
+
+        start = 81
+        for value in stringEasy9:
+            if int(value) == 0:
+                start -= 1
+
+        puzzleEasy9 = sudoku(9,3,3,stringEasy9)
+        if full:
+            print "================================"
+            print puzzleEasy9
+        solver(puzzleEasy9)
+        if full:
+            print puzzleEasy9
+            print puzzleEasy9.ghostData
+
+        end = 81
+        for value in puzzleEasy9.data.itervalues():
+            if value == 0:
+                end -= 1
+
+        results[i + 1] = [start, end]
+    
+    print            
+    return results
+
+def solver(puzzle):
+    methods = [puzzle.nakedSingle, puzzle.hiddenSingle, puzzle.nakedTwin]
+    puzzle.changes = True
+    while puzzle.changes:
+        for method in range(len(methods)):
+            methods[method]()
+            
+
+string9 = "030647080709000206010903040301070804800304002402050603080501020103000409020439060"
+string9 = "100920000524010000000000070050008102000000000402700090060000000000030945000071006"
+##string9 = "904200007010000000000706500000800090020904060040002000001607000000000030300005702"
+string9 = "400270600\
+798156234\
+020840007\
+237468951\
+849531726\
+561792843\
+082015479\
+070024300\
+004087002"
+
+string8 = "1004200006000705005008006800100000060052004006008060007000073006"
+
+string6 = "020000000020054100100064200043300010"
+##string6 = "123456123456123456123456123456123456"
+
+puzzle9 = sudoku(9,3,3,string9)
+
+puzzle8 = sudoku(8,2,4,string8)
+
+puzzle6 = sudoku(6,2,3,string6)
+
+# puzzle9.nakedTwin()
+print puzzle9
+print puzzle9.data
+# puzzle9.populateGhosts()
+# print puzzle9.ghostData
+# puzzle9.nakedTwin()
+# print puzzle9.ghostData
+# print puzzle9
+
+##print easy(True)
