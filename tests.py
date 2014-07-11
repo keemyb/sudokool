85a1f92aefd23a488e05291f31758fa41189d9a4
Akeem Brown <akeemb@gmail.com>
2014-07-11 01:58:34 +0100

    Refactored tests
    The solver function now has the ability to provide snapshosts
    of the solving progress after each method is run.
    Unfortunately the string representing the puzzle that is
    passed becomes garbled.
    
    created a puzzleSummary function that provides useful info
    about a puzzle, such as showing the puzzle before and after
    a solving attempt, as well as the amount of values updated
    and any updated ghostValues. It also shows the order in which
    solving methods were executed (via the history of solver).
    
    fromText has now been reduced to a helper that only passes
    select puzzles from a text file to puzzleSummary.
    All the analytical parts of fromText have been moved and
    enhanced in puzzleSummary, in an attempt to make the system
    more modular.

--- tests.py
+++ tests.py
@@ -1,95 +1,76 @@
 from sudoku import sudoku
 from operator import add, or_
+from math import sqrt
+from copy import deepcopy
 
-def fromText(textfile, n, level, onlyShowSolved, ghostValues = True):
+def puzzleSummary(puzzle, maxLevel, printPuzzle, breakdown, printGhostValues, no = None):
+
+    preSolved = puzzle
+    postSolved = deepcopy(preSolved)
+
+    solveReport = solver(postSolved, maxLevel, breakdown)
+
+    numberOfPreSolvedValues = reduce(add, [1 for value in preSolved.values.itervalues() if value != 0], 0)
+    numberOfPostSolvedValues = reduce(add, [1 for value in postSolved.values.itervalues() if value != 0], 0)
+
+    if no == None:
+        no = 1
+
+    if printPuzzle:
+        printPuzzle = str(preSolved) + "\n" + str(postSolved) + "\n"
+    else:
+        printPuzzle = ""
+
+    changedGhostsString = ""
+    if printGhostValues:
+        for key in postSolved.ghostValues.iterkeys():
+            if postSolved.ghostValues[key] != preSolved.ghostValues[key]:
+                changedGhostsString += "\n" + str(key) + " " + str(preSolved.ghostValues[key]) + " " + str(postSolved.ghostValues[key])
+
+    return str(no) + " " + str(numberOfPreSolvedValues) + " ---> " + str(numberOfPostSolvedValues) + "\n" + str(solveReport[1]) + str(solveReport[2]) + str(changedGhostsString) + "\n" + printPuzzle + "\n" * 2
+
+def fromText(textfile, seperator, maxLevel, n, specific, breakdown, showModified, showSolved, printPuzzle, printGhostValues):
     
     fileToRead = open(textfile, "r")
     currentPuzzle = ""
     puzzles = []
+    
     for line in fileToRead:
-        if len(puzzles) < n:
-            if line != "========\n":
-                currentPuzzle += line.strip()
-            else:
+        if len(puzzles) <= n:
+            if line == seperator:
                 puzzles.append(currentPuzzle)
                 currentPuzzle = ""
+            else:
+                currentPuzzle += line.strip()
+    if len(currentPuzzle) == len(puzzles[0]):
+        puzzles.append(currentPuzzle)
+
+    if specific:
+        puzzles = [puzzles[n - 1]]
+        no = n
+    else:
+        no = 1
 
     results = ""
-    no = 1
 
-    for puzzle in puzzles:
-        preSolved = sudoku(9,3,3,puzzle)
-        postSolved = sudoku(9,3,3,puzzle)
-
-        print solver(postSolved, level)
-
-        numberOfPreSolvedValues = reduce(add, [1 for value in preSolved.values.itervalues() if value != 0], 0)
-        numberOfPostSolvedValues = reduce(add, [1 for value in postSolved.values.itervalues() if value != 0], 0)
-
-        changedGhosts = {}
-        for key in postSolved.ghostValues.iterkeys():
-            if postSolved.ghostValues[key] != preSolved.ghostValues[key]:
-                changedGhosts[key] = postSolved.ghostValues[key], preSolved.ghostValues[key]
-
-        results += str(no) + " " + str(numberOfPreSolvedValues) + " " + str(numberOfPostSolvedValues) + " " + str(changedGhosts) + "\n"
+    for puzzleString in puzzles:
+        gridSize = int(sqrt(len(puzzleString)))
+        subGridsX = int(sqrt(gridSize))
+        puzzle = sudoku(gridSize, subGridsX, subGridsX, puzzleString)
+        results += puzzleSummary(puzzle, maxLevel, printPuzzle, breakdown, printGhostValues)
         no += 1
 
     return results
 
-    # for i in xrange(1):
-    #     start = (i * 89)
-    #     end = start + 9 * 9 
-
-    #     stringEasy9 = stringEasy[start : end]
-
-    #     pre, post = sudoku(9,3,3,stringEasy9), sudoku(9,3,3,stringEasy9)
-
-    #     start = 81
-    #     for value in pre.values.itervalues():
-    #         if value == 0:
-    #             start -= 1
-
-    #     solver(post)
-    #     preGhost = {}
-    #     postGhost = {}
-    #     for key in post.ghostValues.iterkeys():
-    #         if post.ghostValues[key] != pre.ghostValues[key]:
-    #             preGhost[key] = pre.ghostValues[key]
-    #             postGhost[key] = post.ghostValues[key]
-
-    #     fullString = "\n" + str(i + 1) + " ======================" + "\n" + str(pre) + str(preGhost) + "\n" + str(post) + str(postGhost)
-    #     liteString = "\n" + str(i + 1) + " ======================" + "\n" + str(pre) + str(post)
-
-    #     if onlyShowSolved:
-    #         if pre.ghostValues != post.ghostValues:
-    #             if ghostValues:
-    #                 print fullString
-    #             else:
-    #                 print liteString
-    #     else:
-    #         if ghostValues:
-    #             print fullString
-    #         else:
-    #             print liteString
-
-
-    #     end = 81
-    #     for value in post.values.itervalues():
-    #         if value == 0:
-    #             end -= 1
-
-
-    #     results[i + 1] = [start, end]
-    
-    # print            
-    # return results
-
-def solver(puzzle, maxLevel, history = None):
+def solver(puzzle, maxLevel, breakdown, history = None):
     methods = [puzzle.nakedSingle, puzzle.hiddenSingle, puzzle.nakedTwin]
 
     #puzzle is complete if gridSize ^ 2 values are filled
     if reduce(add, [1 for value in puzzle.values.itervalues() if value != 0], 0) == puzzle.gridSize ** 2:
-        return True, [entry[0] for entry in history if history != None]
+        if breakdown:
+            return False, [entry[0] for entry in history if history != None], [entry[2] for entry in history if history != None]
+        else:
+            return False, [entry[0] for entry in history if history != None], ""
 
     if maxLevel > len(methods):
         maxLevel = len(methods)
@@ -97,28 +78,34 @@ def solver(puzzle, maxLevel, history = None):
     #if solver is run for the first time, solve using first method
     if history == None:
         methods[0]()
-        history = [(0, puzzle.changes)]
-        return solver(puzzle, maxLevel, history)
-    
-    # #check to see if last couple attempts were all unsuccessful, if so, quit
-    # elif len(history) > len(methods):
-    #     if reduce(or_, [boolean[1] for boolean in history][-(len(methods)):]) == False:
-    #         return False
+        if breakdown:
+            history = [(0, puzzle.changes, str(puzzle))]
+        else:
+            history = [(0, puzzle.changes)]
+        return solver(puzzle, maxLevel, breakdown, history)
     
     #if last attempt was successful, go back to first level
+
     lastMethod = history[-1][0]
     if history[-1][1] == True:
         nextMethod = 0
     #or if unsuccessful, increase level or exit if highest level was tried
     else:
         if lastMethod == maxLevel - 1:
-            return False, [entry[0] for entry in history if history != None]
+            if breakdown:
+                return False, [entry[0] for entry in history if history != None], [entry[2] for entry in history if history != None]
+            else:
+                return False, [entry[0] for entry in history if history != None], ""
         else:
             nextMethod = lastMethod + 1
 
     methods[nextMethod]()
-    history.append((nextMethod, puzzle.changes))
-    return solver(puzzle, maxLevel, history)
+    if breakdown:
+        history.append((nextMethod, puzzle.changes, str(puzzle)))
+    else:
+        history.append((nextMethod, puzzle.changes))
+    
+    return solver(puzzle, maxLevel, breakdown, history)
 
 string9 = "030647080709000206010903040301070804800304002402050603080501020103000409020439060"
 string9 = "100920000524010000000000070050008102000000000402700090060000000000030945000071006"
@@ -153,19 +140,7 @@ puzzle8 = sudoku(8,2,4,string8)
 
 puzzle6 = sudoku(6,2,3,string6)
 
-# for puzzle in [puzzle9, puzzle8]:
-#     # puzzle.nakedSingle()
-#     # print puzzle
-#     # print puzzle.ghostValues
-#     # puzzle.nakedTwin()
-#     # print puzzle
-#     # print puzzle.ghostValues
+for puzzle in [puzzle9]:
+    print puzzleSummary(puzzle, 3, True, True, True)
 
-#     print puzzle
-#     print puzzle.ghostValues
-#     puzzle.nakedTwin()
-#     print puzzle
-#     print puzzle.ghostValues
-
-print fromText("easy50.txt", 1, 3, True)
-# print fromText("easy50.txt", 50, 1, True) == fromText("easy50.txt", 50, 2, True)
+# print fromText("easy50.txt", "========\n", 3, 50, True, False, True, True, True, True)
