557a8e48cc936183436945ef7eba049b82387dce
Akeem Brown <akeemb@gmail.com>
2014-09-11 14:38:04 +0100

    flow: Created branch 'feature/Remote Pairs'.

---
+++ gameLoop.py
@@ -0,0 +1,105 @@
+import kivy
+kivy.require('1.8.0')  # replace with your current kivy version !
+
+from kivy.app import App
+from sudoku import Sudoku
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+
+
+class screen(BoxLayout):
+
+    def __init__(self, horizontalFormat=True, **kwargs):
+        super(screen, self).__init__(**kwargs)
+
+        if horizontalFormat:
+            self.orientation = "horizontal"
+        else:
+            self.orientation = "vertical"
+        self.add_widget(puzzle())
+        self.add_widget(inputs())
+
+
+class puzzle(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(puzzle, self).__init__(**kwargs)
+
+        self.cols = sudoku.unitSize()
+
+        for location in self.locations:
+            if sudoku.isEmpty(location):
+                self.add_widget(candidates(location))
+            else:
+                value = str(sudoku.getLocationValue(location))
+                self.add_widget(Label(text=value, font_size=40))
+
+
+class candidates(GridLayout):
+
+    def __init__(self, location, userCandidates=False, **kwargs):
+        super(candidates, self).__init__(**kwargs)
+
+        self.cols = sudoku.subGridsInRow()
+
+        if userCandidates:
+            if sudoku.hasUserCandidates(location):
+                for candidate in sudoku.userCandidates(location):
+                    self.add_widget(Label(text=str(candidate)))
+        else:
+            if sudoku.hasSolvingCandidates(location):
+                for candidate in sudoku.solvingCandidates(location):
+                    self.add_widget(Label(text=str(candidate)))
+
+
+class inputs(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(inputs, self).__init__(**kwargs)
+
+        self.cols = sudoku.subGridsInRow()
+
+        for value in sudoku.possibleValues():
+            self.add_widget(Button(text=str(value)))
+
+
+class SudokuApp(App):
+
+    def build(self):
+        return screen(False)
+
+string9 = "003020600\
+900305001\
+001806400\
+008102900\
+700000008\
+006708200\
+002609500\
+800203009\
+005010300"
+sudoku = Sudoku(string9)
+
+# string16 = "B07805E0300AD0CG\
+# 004007000C0FA002\
+# A000000000043700\
+# 0050009F00000008\
+# 0400B8000E079300\
+# 00E37C0000FDB004\
+# 9F07005D03000080\
+# 500D0F3024A8C0G0\
+# 08000000B0000GD5\
+# 00D000000800F0E0\
+# 00A090F0067000BC\
+# 000C0AB0000E7240\
+# 7A090B1000500630\
+# D0CEF070A0000800\
+# 0000E0A00D005000\
+# 0635G9C00B00E000"
+
+# sudoku = Sudoku(string16)
+sudoku.initialiseIntersections()
+
+if __name__ == '__main__':
+    SudokuApp().run()
---
+++ tests.py
@@ -0,0 +1,139 @@
+from sudoku import Sudoku
+from puzzleSummary import puzzleSummary
+
+def fromTextPuzzleSummary(textfile, seperator, maxLevel, n, specific, showModified, showSolved, printPuzzle, printGhostValues, printHistory):
+
+    fileToRead = open(textfile, "r")
+    currentPuzzle = ""
+    puzzles = []
+
+    for line in fileToRead:
+        if len(puzzles) <= n:
+            if line == seperator:
+                puzzles.append(currentPuzzle)
+                currentPuzzle = ""
+            else:
+                currentPuzzle += line.strip()
+    if len(currentPuzzle) == len(puzzles[0]):
+        puzzles.append(currentPuzzle)
+
+    if specific:
+        puzzles = [puzzles[n - 1]]
+        no = n
+    else:
+        no = 1
+
+    results = ""
+
+    for puzzleString in puzzles:
+        puzzle = Sudoku(puzzleString, True)
+        results += puzzleSummary(puzzle, maxLevel, printPuzzle, printGhostValues, printHistory, no)
+        no += 1
+
+    return results
+
+def fromTextToPuzzle(textfile, seperator, n):
+    fileToRead = open(textfile, "r")
+    currentPuzzle = ""
+    puzzles = []
+
+    for line in fileToRead:
+        if len(puzzles) <= n:
+            if line == seperator:
+                puzzles.append(currentPuzzle)
+                currentPuzzle = ""
+            else:
+                currentPuzzle += line.strip()
+    if len(currentPuzzle) == len(puzzles[0]):
+        puzzles.append(currentPuzzle)
+
+    puzzle = puzzles[n - 1]
+    return Sudoku(puzzle, True)
+
+def visualizer(puzzle, *groups):
+    wholeString = ""
+
+    for group in groups:
+        gridSize = puzzle.gridSize
+        subGridsX = puzzle.subGridsX
+        subGridsY = puzzle.subGridsY
+        string = ""
+        vPipe = "="
+        hPipe = "="
+        # first line to accommodate vertical pipe and following spaces (minus one to account for last v pipe)
+        # second line for numbers (and following spaces)
+        hPipeString = hPipe * ((len(vPipe) + 1) * (subGridsX + 1) - 1) + \
+        hPipe * (gridSize * 2) + \
+        "\n"
+
+        for position in xrange(1, gridSize ** 2 + 1):
+
+            if (position - 1) % (gridSize * subGridsX) == 0:
+                string += hPipeString
+
+            if (position - 1) % subGridsY == 0 :
+                string += vPipe + " "
+
+            if position not in group:
+                string += "  "
+            else:
+                string += "0 "
+
+            if position % gridSize == 0:
+                string += vPipe + "\n"
+
+            if position == gridSize ** 2:
+                string += hPipeString
+
+        wholeString += string
+
+    return wholeString
+
+xyzWing = Sudoku("600000008500908007820001030340209080200080300180307025750400092900005004400090003")
+# xyzWing = Sudoku("092001750500200008000030200075004960200060075069700030008090020700003089903800040")
+print puzzleSummary(xyzWing, 0, True, True, True)
+# for group in xyzWing.generateXYZWingGroups():
+#     locations = group[0]
+#     print visualizer(xyzWing, locations)
+#     print locations
+# xyzWing.solve(0)
+# print xyzWing
+
+
+# print puzzleSummary(conjugatePairPuzzle, 0, True, True, True)
+
+# for puzzleNo in [6,7,42,47,48,49,50]:
+# for puzzleNo in xrange(5,11):
+# for puzzleNo in xrange(1,51):
+    # print fromTextPuzzleSummary("easy50.txt", "========\n", 0, puzzleNo, True, True, True, True, True, True)
+
+# string16 = "B07805E0300AD0CG\
+# 004007000C0FA002\
+# A000000000043700\
+# 0050009F00000008\
+# 0400B8000E079300\
+# 00E37C0000FDB004\
+# 9F07005D03000080\
+# 500D0F3024A8C0G0\
+# 08000000B0000GD5\
+# 00D000000800F0E0\
+# 00A090F0067000BC\
+# 000C0AB0000E7240\
+# 7A090B1000500630\
+# D0CEF070A0000800\
+# 0000E0A00D005000\
+# 0635G9C00B00E000"
+
+# puzzle16 = Sudoku(string16)
+# print puzzleSummary(puzzle16, 0, True, True, True)
+
+# fileToRead = open("top95.txt", "r")
+# number = 1
+# for line in fileToRead:
+#     try:
+#         puzzle = Sudoku(str(line)[:81])
+#         print puzzleSummary(puzzle, 0, True, True, True, number)
+#     except:
+#         continue
+#     else:
+#         number += 1
