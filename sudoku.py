dbc97d2374a38cfa852d56a909462ce83cdf4547
Akeem Brown <akeemb@gmail.com>
2014-10-11 15:19:42 +0100

    Changed group of candidates added to undo stack

--- sudoku.py
+++ sudoku.py
@@ -1547,7 +1547,7 @@ class Sudoku():
                 removedCandidates.append(candidate)
 
         if removedCandidates:
-            self.addInverseActionToUndoStack(self.addSolvingCandidates, location, candidates)
+            self.addInverseActionToUndoStack(self.addSolvingCandidates, location, removedCandidates)
             self.changes = True
 
         return removedCandidates
