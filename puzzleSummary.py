297c2a1741bb2118f76bef7c1d8b43d1be26c138
Akeem Brown <akeemb@gmail.com>
2014-11-02 12:50:58 +0000

    flow: Created branch 'feature/Refactor generation sudoku'.

---
+++ puzzleSummary.py
@@ -0,0 +1,42 @@
+from copy import deepcopy
+
+def puzzleSummary(puzzle, maxLevel, printPuzzle, printChangedCandidates, printHistory, number=None, verbose=True):
+
+    puzzle.initialiseIntersections()
+    preSolved = deepcopy(puzzle)
+    postSolved = puzzle
+
+    postSolved.solve(maxLevel)
+
+    numberOfPreSolvedValues = len(preSolved.filledLocations())
+    numberOfPostSolvedValues = len(postSolved.filledLocations())
+
+    if number is None:
+        number = ""
+    else:
+        number = str(number) + " "
+
+    if not postSolved.isValid():
+        valid = "\n" + "Puzzle Invalid" + "\n"
+    else:
+        valid = "\n"
+
+    if printPuzzle:
+        printPuzzle = "\n" + str(preSolved) + valid + str(postSolved) + "\n"
+    else:
+        printPuzzle = valid
+
+    changedCandidatesString = ""
+    if printChangedCandidates and not postSolved.isComplete():
+        for key in postSolved.solvingCandidatesDict.iterkeys():
+            if postSolved.solvingCandidatesDict[key] != preSolved.solvingCandidatesDict[key] and postSolved.solvingCandidatesDict[key]:
+                changedCandidatesString += "\n" + str(key) + " " + str(preSolved.solvingCandidatesDict[key]) + " " + str(postSolved.solvingCandidatesDict[key])
+
+    history = ""
+    if printHistory:
+        if verbose:
+            history = "\n" + "\n".join(postSolved.log)
+        else:
+            history = "\n" + str(postSolved.history)
+
+    return number + str(numberOfPreSolvedValues) + " ---> " + str(numberOfPostSolvedValues) + history + changedCandidatesString + printPuzzle + "\n"
