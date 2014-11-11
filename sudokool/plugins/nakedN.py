96fcee7b5820a290e189cb1efef0f135f412f5a3
Akeem Brown <akeemb@gmail.com>
2014-11-11 18:05:34 +0000

    Moved description attribute to docstring (naked N)

--- sudokool/plugins/nakedN.py
+++ sudokool/plugins/nakedN.py
@@ -1,14 +1,15 @@
 from sudokool.plugin import Plugin
 
 class __nakedN(Plugin):
+    '''Naked N
+
+    This plugin looks for n cells in every intersection that have n, and only n
+    candidates in common. If these are found, these n candidates are removed
+    from every other location in the intersection.
+    '''
 
     def __init__(self):
         self.name = "Naked N"
-        self.description ='''
-        This plugin looks for n cells in every intersection that have n, and 
-        only n candidates in common. If these are found, these n candidates are
-        removed from every intersection all n cells have in common.
-        '''
         self.minSize = None
         self.maxSize = None
         self.rank = 0
@@ -49,14 +50,15 @@ class __nakedN(Plugin):
                             puzzle.addToLog(successString, removedCandidates, location, puzzle.alignment(*combination)[0], combination)
 
 class nakedTwin(__nakedN):
+    '''Naked Twin
+
+    This plugin looks for 2 cells in every intersection that have 2, and only 2
+    candidates in common. If these are found, these 2 candidates are removed
+    from every other location in the intersection.
+    '''
 
     def __init__(self):
         self.name = "Naked Twin"
-        self.description ='''
-        This plugin looks for 2 cells in every intersection that have 2, and 
-        only 2 candidates in common. If these are found, these 2 candidates are
-        removed from every intersection both cells have in common. 
-        '''
         self.minSize = None
         self.maxSize = None
         self.rank = 20
@@ -65,14 +67,15 @@ class nakedTwin(__nakedN):
         return super(nakedTwin, self).solve(puzzle, 2)
 
 class nakedTriple(__nakedN):
+    '''Naked Triple
+
+    This plugin looks for 3 cells in every intersection that have 3, and only 3
+    candidates in common. If these are found, these 3 candidates are removed
+    from every other location in the intersection.
+    '''
 
     def __init__(self):
         self.name = "Naked Triple"
-        self.description ='''
-        This plugin looks for 3 cells in every intersection that have 3, and 
-        only 3 candidates in common. If these are found, these 3 candidates are
-        removed from every intersection all 3 cells have in common. 
-        '''
         self.minSize = 3
         self.maxSize = None
         self.rank = 60
